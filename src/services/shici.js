f663f25dbf31b580181d3c7b3aa36b064c77e0df
youling4438 <youling0106@qq.com>
2018-12-27 00:32:42 +0800

    fixed sagas

--- src/services/shici.js
+++ src/services/shici.js
@@ -1,11 +1,11 @@
 
 export default {
     change() {
-        try {
-            fetch("https://api.gushi.ci/all.json").then((result) => result.json());
-        }
-        catch (err) {
-            throw new Error(err);
-        }
+        // try {
+        fetch("https://api.gushi.ci/all.json").then((result) => result.json());
+        // }
+        // catch (err) {
+        // throw new Error(err);
+        // }
     },
 }
