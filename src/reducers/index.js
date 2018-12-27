f08323e998f7b0d1558de20d4587f14eed7c9535
youling4438 <youling0106@qq.com>
2018-12-27 23:52:22 +0800

    remove log info

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,6 @@ export default function counter(state = intlState, action) {
   switch (action.type) {
     case 'CHANGE_SHICI_RESP':
       const { shici } = action;
-      console.log(action);
       return {
         shici
       }
