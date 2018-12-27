03b2bcd9c51077fbc728d267413aff0b0522d661
youling4438 <youling0106@qq.com>
2018-12-27 22:49:00 +0800

    delete sagas counter

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,12 +3,6 @@
 import { put, takeEvery, delay, call } from 'redux-saga/effects'
 import api from '../services/shici'
 
-export function* incrementAsync() {
-  yield delay(1000)
-
-  yield put({ type: 'INCREMENT' })
-}
-
 export function* changeShici() {
   const shici = '纵使晴明无雨色，入云深处亦沾衣'
   try {
