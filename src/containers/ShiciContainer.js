a37d2bce94141cd377ece39753805e21d20b025a
youling4438 <youling0106@qq.com>
2018-12-30 23:45:45 +0800

    import action replease type function at main.js file

---
+++ src/containers/ShiciContainer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Shici } from '../components/'
+import { createStructuredSelector, createSelector } from 'reselect'
+
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as shiciAction from '../actions/Shici'
+
+class ShiciContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.changeShici = this.changeShici.bind(this)
+    }
+    changeShici() {
+        console.log('changeShici -------------------------');
+        this.props.changeShici()
+    }
+    render() {
+        const { shiciData } = this.props;
+        console.log('-----containter render------', this.props);
+        return (
+            <Shici
+                shiciData={shiciData}
+                changeShici={this.changeShici}
+            />
+        )
+    }
+}
+
+const mapStateToProps = createStructuredSelector({
+    shiciData: createSelector(
+        (state) => state.shiciData,
+        (shiciState) => shiciState
+    ),
+})
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(shiciAction, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShiciContainer)
