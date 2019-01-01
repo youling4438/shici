f9754c20ac82229753f7b194065b6a9967ee0ea5
youling4438 <youling0106@qq.com>
2019-01-01 17:08:59 +0800

    remove code from main.js

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { Provider } from 'react-redux'
-import Shici from './components/Shici'
-import Header from './components/Header'
 import reducer from './reducers'
 import Routes from './routes'
 import rootSaga from './sagas'
@@ -16,17 +14,8 @@ const sagaMiddleware = createSagaMiddleware()
 const store = createStore(reducer, applyMiddleware(sagaMiddleware))
 sagaMiddleware.run(rootSaga)
 
-// const action = type => store.dispatch({ type })
-
 function render() {
   ReactDOM.render(
-    // <div>
-    //   <Header />
-    //   <Shici
-    //     shiciData={store.getState().shiciData}
-    //     changeShici={() => action('CHANGE_SHICI')}
-    //   />
-    // </div>,
     <div>
       <Globalstyle />
       <Provider store={store}>
