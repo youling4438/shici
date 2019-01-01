099c5b678469958c8ef9f4cb13b7b7e47c950353
youling4438 <youling0106@qq.com>
2019-01-01 19:00:38 +0800

    修改action的写法

--- src/actions/Shici.js
+++ src/actions/Shici.js
@@ -2,5 +2,5 @@ export const CHANGE_SHICI = 'CHANGE_SHICI'
 export const CHANGE_SHICI_RESP = 'CHANGE_SHICI_RESP'
 
 export const changeShici = () => ({
-    'type': 'CHANGE_SHICI'
+    'type': CHANGE_SHICI
 })
