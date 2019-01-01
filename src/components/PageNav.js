74a6607c90cfbcb8c8a87250a6deadec085712ba
youling4438 <youling0106@qq.com>
2019-01-01 18:56:53 +0800

    增加新的组建和APP首页路由

--- src/components/PageNav.js
+++ src/components/PageNav.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Buttonstyle } from '../styles/global-styles'
 
+const PageNavContent = styled.div`
+    margin: 40px auto;
+    text-align: center;
+`
 const PageNav = ({ buttonClick }) => (
-    <h2 onClick={() => buttonClick('shici')}>精选古诗词</h2>
+    <PageNavContent>
+        <Buttonstyle />
+        <div className={'button'} onClick={() => buttonClick('/')}>精选APP</div>
+        <div className={'button'} onClick={() => buttonClick('/shici')}>精选古诗词</div>
+        <div className={'button'} onClick={() => buttonClick('/hitokoto')}>精选一言</div>
+    </PageNavContent>
 )
 
 export default PageNav
