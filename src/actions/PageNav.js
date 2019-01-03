8405616f932f9d49d752afd3ed74715bc5fd9247
youling <youling0106@qq.com>
2019-01-03 22:33:55 +0800

    modify change router function

--- src/actions/PageNav.js
+++ src/actions/PageNav.js
@@ -1,5 +1,8 @@
-import history from '../history';
+import history from '../history'
+
+export const CHANGE_ROUTER = 'CHANGE_ROUTER'
 
 export const changeRouter = (router) => {
     history.push(router);
+    return { 'type': CHANGE_ROUTER }
 }
