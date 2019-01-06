39e3bf9930449e7ab7e29ced4098dc4c9a04f2b5
youling <youling0106@qq.com>
2019-01-06 23:19:29 +0800

    add function to watch jr action

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,15 +3,21 @@
 import { put, takeEvery, call } from 'redux-saga/effects'
 import { CHANGE_SHICI_RESP, CHANGE_SHICI } from '../actions/Shici'
 import { CHANGE_HITOKOTO_RESP, CHANGE_HITOKOTO } from '../actions/Hitokoto'
+import { CHANGE_JR_SHICI_RESP, CHANGE_JR_SHICI } from '../actions/Jrsc'
 import shiciApi from '../services/shici'
 import hitokotoApi from '../services/hitokoto'
+import jrshiciApi from '../services/jrshici'
 
 function getShici() {
-  return shiciApi.get().then(resp => resp)
+    return shiciApi.get().then(resp => resp)
 }
 
 function gethitokoto() {
-  return hitokotoApi.get().then(resp => resp)
+    return hitokotoApi.get().then(resp => resp)
+}
+
+function getJrShici() {
+    return jrshiciApi.get().then(resp => resp)
 }
 
 export function* changeShici() {
@@ -38,7 +44,20 @@ export function* changeWord() {
   }
 }
 
+export function* changeJrShici() {
+    const defaultShici = '纵使晴明无雨色，入云深处亦沾衣'
+    try {
+        const data = yield call(getJrShici);
+        const { data: jrshiciData } = JSON.parse(data);
+        console.log('jrshiciData :', jrshiciData);
+        yield put({ type: CHANGE_JR_SHICI_RESP, jrshiciData });
+    } catch (error) {
+        yield put({ type: CHANGE_JR_SHICI_RESP, jrshiciData });
+    }
+}
+
 export default function* rootSaga() {
-  yield takeEvery(CHANGE_SHICI, changeShici)
-  yield takeEvery(CHANGE_HITOKOTO, changeWord)
+    yield takeEvery(CHANGE_SHICI, changeShici)
+    yield takeEvery(CHANGE_HITOKOTO, changeWord)
+    yield takeEvery(CHANGE_JR_SHICI, changeJrShici)
 }
