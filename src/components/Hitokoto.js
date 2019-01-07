38daf049dc3cefe3a9d91a1e1f21052fd899f903
youling4438 <youling0106@qq.com>
2019-01-07 23:51:47 +0800

    use the common components

--- src/components/Hitokoto.js
+++ src/components/Hitokoto.js
@@ -1,52 +1,9 @@
 import * as React from 'react'
-import styled from 'styled-components'
+import { Intro, ChangeButton } from './common'
 import { Containerstyle } from '../styles/global-styles'
 import collectedIcon from '../assets/collected.svg'
 
-const Intro = styled.p`
-    font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-    font-size: 40px;
-    text-align: center;
-        .from{
-            margin-top: 20px;
-            text-align: center;
-            font-size: 20px;
-            font-weight: bold;
-            color: #5c9291;
-            .collect, .collected{
-                float: right;
-                cursor: pointer;
-                filter: brightness(0);
-                img{
-                    padding: 5px 10px;
-                    width: 25px;
-                }
-            }
-            .collect:hover, span.collected{
-                filter: brightness(1);
-            }
-        }
-    `
-const ChangeButton = styled.div`
-  font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-size: 20px;
-  text-align: center;
-  margin-bottom: 20px;
-
-  .changeButton{
-    cursor:pointer;
-    color: #000;
-    border-bottom: 1px dashed currentColor;
-    margin: 0 15px;
-  }
-
-  .changeButton:hover{
-    border-bottom-style: solid;
-  }
-`
-
 const Hitokoto = ({ hitokotoData, showPrevButton, showNextButton, changeHitokoto, collectHitokoto, prevHitokoto, nextHitokoto }) => {
-    console.log('hitokotoData: ', hitokotoData);
     const { hitokoto, from, creator, id, created_at, type, collect, orderNumber } = hitokotoData;
     const collectHitokotoHandle = () => {
         collectHitokoto(orderNumber);
--- src/components/Shici.js
+++ src/components/Shici.js
@@ -1,51 +1,7 @@
 import * as React from 'react'
-import styled from 'styled-components'
+import { Intro, ChangeButton } from './common'
 import { Containerstyle } from '../styles/global-styles'
 import collectedIcon from '../assets/collected.svg'
-
-const Intro = styled.p`
-    font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-    font-size: 40px;
-    text-align: center;
-        .from{
-            margin-top: 20px;
-            text-align: center;
-            font-size: 20px;
-            font-weight: bold;
-            line-height: 35px;
-            color: #5c9291;
-            .collect, .collected{
-                float: right;
-                cursor: pointer;
-                filter: brightness(0);
-                img{
-                    padding: 5px 10px;
-                    width: 25px;
-                }
-            }
-            .collect:hover, span.collected{
-                filter: brightness(1);
-            }
-        }
-
-    `
-const ChangeButton = styled.div`
-  font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-size: 20px;
-  text-align: center;
-  margin-bottom: 20px;
-
-  .changeButton{
-    cursor: pointer;
-    color: #000;
-    border-bottom: 1px dashed currentColor;
-    margin: 0 15px;
-  }
-
-  .changeButton:hover{
-    border-bottom-style: solid;
-  }
-`
 
 const Shici = ({ shiciData, showPrevButton, showNextButton, changeShici, collectShici, prevShici, nextShici }) => {
     const { content, author, origin, category, collect, orderNumber } = shiciData;
