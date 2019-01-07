5709683dbca82a87323a38fcdabfdf6035563d13
youling4438 <youling0106@qq.com>
2019-01-07 23:27:18 +0800

    add tags container

--- src/components/Jrsc.js
+++ src/components/Jrsc.js
@@ -7,28 +7,28 @@ const Intro = styled.p`
     font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
     font-size: 40px;
     text-align: center;
-        .from{
-            margin-top: 20px;
-            text-align: center;
-            font-size: 20px;
-            font-weight: bold;
-            line-height: 35px;
-            color: #5c9291;
-            .collect, .collected{
-                float: right;
-                cursor: pointer;
-                filter: brightness(0);
-                img{
-                    padding: 5px 10px;
-                    width: 25px;
-                }
-            }
-            .collect:hover, span.collected{
-                filter: brightness(1);
-            }
-        }
+	.from{
+		margin-top: 20px;
+		text-align: center;
+		font-size: 20px;
+		font-weight: bold;
+		line-height: 35px;
+		color: #5c9291;
+		.collect, .collected{
+			float: right;
+			cursor: pointer;
+			filter: brightness(0);
+			img{
+				padding: 5px 10px;
+				width: 25px;
+			}
+		}
+		.collect:hover, span.collected{
+			filter: brightness(1);
+		}
+	}
+`
 
-    `
 const ChangeButton = styled.div`
     font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
     font-size: 20px;
@@ -46,6 +46,11 @@ const ChangeButton = styled.div`
         border-bottom-style: solid;
     }
 `
+
+const TagContainer = styled.p`
+	text-align: center;
+`
+
 const Tag = styled.span`
 	font-size: 16px;
 	color: #5c9291;
@@ -78,7 +83,7 @@ function Jrsc({ jrshiciData, changeJrShici, collectJrShici }) {
 						------{author} {dynasty} {title}
 						<span className={collect ? 'collected' : 'collect'}><img src={collectedIcon} onClick={collectJrShiciHandle} /></span>
 					</div>
-					{matchTagsDom}
+					<TagContainer>{matchTagsDom}</TagContainer>
 				</Intro>
 				<ChangeButton>
 					<a className={'changeButton'} onClick={changeJrShici}>随机诗词</a>
