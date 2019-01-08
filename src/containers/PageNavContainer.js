59be7547750fd2796a204b9265f62722e3a8c945
youling4438 <youling0106@qq.com>
2019-01-08 23:58:13 +0800

    remove log info

--- src/containers/PageNavContainer.js
+++ src/containers/PageNavContainer.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { PageNav } from '../components/'
-// import { createStructuredSelector, createSelector } from 'reselect'
-
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import * as pageNavAction from '../actions/PageNav'
@@ -12,7 +10,6 @@ class PageNavContainer extends React.Component {
         this.bottonClickHandle = this.bottonClickHandle.bind(this)
     }
     bottonClickHandle(router) {
-        console.log('changeRouter -------------------------');
         this.props.changeRouter(router)
     }
     render() {
