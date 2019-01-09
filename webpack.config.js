bc0825cd280943e1b468e0ca6573d4f12579f2bb
youling <youling0106@qq.com>
2019-01-09 21:49:31 +0800

    format webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,31 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src/main.js'),
-  output: {
-    path: __dirname + "/build",
-    filename: "bundle-[hash].js"
-  },
-  devtool: 'eval-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.svg$/,
-        use: ['file-loader'],
-      },
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/main.js'),
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle-[hash].js"
+    },
+    devtool: 'eval-source-map',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') }),
     ],
-  },
-  devServer: {
-    port: 3000,
-    hot: true,
-  },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: ['file-loader'],
+            },
+        ],
+    },
+    devServer: {
+        port: 3000,
+        hot: true,
+    },
 }
