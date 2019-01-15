03c0a35c8530b906831e4825394edf1e6cdd1515
youling4438 <youling0106@qq.com>
2019-01-15 23:40:49 +0800

    remove pagenav action

--- src/actions/PageNav.js
+++
@@ -1,8 +0,0 @@
-import history from '../history'
-
-export const CHANGE_ROUTER = 'CHANGE_ROUTER'
-
-export const changeRouter = (router) => {
-    history.push(router);
-    return { 'type': CHANGE_ROUTER }
-}
