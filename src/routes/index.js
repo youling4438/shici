920d22f7bc4e74375a55c93fb2e376d951160aed
youling4438 <youling0106@qq.com>
2019-01-15 23:59:06 +0800

    add router config for detail

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Router, Route } from 'react-router-dom'
 import history from '../history';
 import { ShiciContainer, PageNavContainer, HitokotoContainer, JrscContainer } from '../containers'
-import { Header } from '../components/'
+import { Header, ItemDetail } from '../components/'
 
 function Routes() {
     return (
@@ -12,6 +12,7 @@ function Routes() {
                 <Route path="/shici" component={ShiciContainer} />
                 <Route path="/jrsc" component={JrscContainer} />
                 <Route path="/hitokoto" component={HitokotoContainer} />
+                <Route path="/detail" component={ItemDetail} />
                 <PageNavContainer />
             </div>
         </Router>
