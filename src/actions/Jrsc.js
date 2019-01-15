f9510237c35127ab6e888a4539e18eab9579eea9
youling4438 <youling0106@qq.com>
2019-01-15 23:29:54 +0800

    add shici detail action

--- src/actions/Jrsc.js
+++ src/actions/Jrsc.js
@@ -3,6 +3,7 @@ export const CHANGE_JR_SHICI_RESP = 'CHANGE_JR_SHICI_RESP'
 export const COLLECT_JR_SHICI = 'COLLECT_JR_SHICI'
 export const PREV_JR_SHICI = 'PREV_JR_SHICI'
 export const NEXT_JR_SHICI = 'NEXT_JR_SHICI'
+export const JR_SHICI_DETAIL = 'JR_SHICI_DETAIL'
 
 export const changeJrShici = () => ({
     'type': CHANGE_JR_SHICI
@@ -21,4 +22,9 @@ export const prevJrShici = (index) => ({
 export const nextJrShici = (index) => ({
     'type': NEXT_JR_SHICI,
     index
+})
+
+export const jrShiciDetail = (jrshici) => ({
+    'type': JR_SHICI_DETAIL,
+    jrshici
 })
