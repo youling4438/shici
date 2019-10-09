ddd1ac0dc2b4bebcb10d23e04aabb1a1ebd5f374
youling4438 <youling0106@qq.com>
2019-10-09 21:58:35 +0800

    :truck: rename router lib file

--- src/containers/JrscContainer.js
+++ src/containers/JrscContainer.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import { Jrsc } from '../components'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import * as jrscAction from '../actions/Jrsc'
-import { showDetail } from '../lib/RouterHeaple'
+import React from "react";
+import { Jrsc } from "../components";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import * as jrscAction from "../actions/Jrsc";
+import { showDetail } from "../lib/RouterHelper";
 
 class JrscContainer extends React.Component {
     constructor(props) {
         super(props);
-        this.changeJrShici = this.changeJrShici.bind(this)
-        this.collectJrShici = this.collectJrShici.bind(this)
-        this.prevJrShici = this.prevJrShici.bind(this)
-        this.nextJrShici = this.nextJrShici.bind(this)
-        this.jrShiciDetail = this.jrShiciDetail.bind(this)
+        this.changeJrShici = this.changeJrShici.bind(this);
+        this.collectJrShici = this.collectJrShici.bind(this);
+        this.prevJrShici = this.prevJrShici.bind(this);
+        this.nextJrShici = this.nextJrShici.bind(this);
+        this.jrShiciDetail = this.jrShiciDetail.bind(this);
     }
     changeJrShici() {
-        this.props.changeJrShici()
+        this.props.changeJrShici();
     }
     collectJrShici(index) {
-        this.props.collectJrShici(index)
+        this.props.collectJrShici(index);
     }
     prevJrShici(index) {
-        this.props.prevJrShici(index)
+        this.props.prevJrShici(index);
     }
     nextJrShici(index) {
-        this.props.nextJrShici(index)
+        this.props.nextJrShici(index);
     }
     jrShiciDetail(jrshici) {
-        this.props.jrShiciDetail(jrshici)
-        showDetail('detail');
+        this.props.jrShiciDetail(jrshici);
+        showDetail("detail");
     }
     render() {
         const { jrshiciData, allJrShiciList } = this.props;
@@ -45,14 +45,17 @@ class JrscContainer extends React.Component {
                 nextJrShici={this.nextJrShici}
                 jrShiciDetail={this.jrShiciDetail}
             />
-        )
+        );
     }
 }
 
-const mapStateToProps = (state) => state.Jrsc
+const mapStateToProps = state => state.Jrsc;
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(jrscAction, dispatch)
+    return bindActionCreators(jrscAction, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(JrscContainer)
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(JrscContainer);
--- src/containers/PageNavContainer.js
+++ src/containers/PageNavContainer.js
@@ -1,22 +1,18 @@
-import React from 'react'
-import { PageNav } from '../components/'
-import { changeRouter } from '../lib/RouterHeaple'
+import React from "react";
+import { PageNav } from "../components/";
+import { changeRouter } from "../lib/RouterHelper";
 
 class PageNavContainer extends React.Component {
     constructor(props) {
         super(props);
-        this.bottonClickHandle = this.bottonClickHandle.bind(this)
+        this.bottonClickHandle = this.bottonClickHandle.bind(this);
     }
     bottonClickHandle(router) {
-        changeRouter(router)
+        changeRouter(router);
     }
     render() {
-        return (
-            <PageNav
-                buttonClick={this.bottonClickHandle}
-            />
-        )
+        return <PageNav buttonClick={this.bottonClickHandle} />;
     }
 }
 
-export default PageNavContainer
+export default PageNavContainer;
