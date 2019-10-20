653fce23862d18a8fc55da3ff3db31dc9171bad5
youling4438 <youling0106@qq.com>
2019-10-20 21:29:36 +0800

    :recycle: adjust navpage

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,28 @@
-import React from 'react'
-import { Router, Route } from 'react-router-dom'
-import history from '../history';
-import { ShiciContainer, PageNavContainer, HitokotoContainer, JrscContainer, ItemDetailContainer } from '../containers'
-import { Header } from '../components/'
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import history from "../history";
+import {
+    ShiciContainer,
+    PageNavContainer,
+    HitokotoContainer,
+    JrscContainer,
+    ItemDetailContainer
+} from "../containers";
+import { Header } from "../components/";
 
 function Routes() {
     return (
         <Router history={history}>
             <div>
                 <Header />
+                <PageNavContainer />
                 <Route path="/shici" component={ShiciContainer} />
                 <Route path="/jrsc" component={JrscContainer} />
                 <Route path="/hitokoto" component={HitokotoContainer} />
                 <Route path="/detail" component={ItemDetailContainer} />
-                <PageNavContainer />
             </div>
         </Router>
-    )
+    );
 }
 
-export default Routes
+export default Routes;
