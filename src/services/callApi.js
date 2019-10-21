f678df156477fd0d3611b4c88fd7a1dd90266634
youling4438 <youling0106@qq.com>
2019-10-21 22:24:02 +0800

    :recycle: modify call api functions

--- src/services/callApi.js
+++ src/services/callApi.js
@@ -1,18 +1,23 @@
-
-export const callFn = ({ url, withCredentials}) => {
+export const callFn = ({
+    url,
+    withCredentials,
+    header,
+    methond = "GET",
+    params
+}) => {
     try {
         return new Promise((resolve, reject) => {
             const xhr = new XMLHttpRequest();
             if (withCredentials) {
                 xhr.withCredentials = true;
             }
-            xhr.open("GET", url);
+            xhr.open(methond, url, true);
+            header && xhr.setRequestHeader("Content-Type", "application/json");
             xhr.onload = () => resolve(xhr.responseText);
             xhr.onerror = () => reject(xhr.statusText);
-            xhr.send();
-        })
-    }
-    catch (err) {
+            xhr.send(methond == "POST" ? JSON.stringify(params) : "");
+        });
+    } catch (err) {
         throw new Error(err);
     }
 };
